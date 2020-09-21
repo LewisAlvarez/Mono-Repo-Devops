f91170cf8a91d504d91f64c73e9438d772962ae4
distribuidos <lewisalvarez99@gmail.com>
2020-09-20 18:23:45 -0700

    Revert "Dockerfile from FrontEnd App"
    This reverts commit 267b3d37d09c12488176ff4ab88ea0013f9c8af4.

--- script-copy.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-##Copiamos toda la carpeta correspondiente al Front donde  se encuentra la carpeta con el ambiente de docker 
-cp -r aik-front Infra/Docker/copy-aik-ui
-echo "Se ha copiado la carpeta del FrontEnd a la carpeta de Infra donde se encuentra el ambiente docker"
