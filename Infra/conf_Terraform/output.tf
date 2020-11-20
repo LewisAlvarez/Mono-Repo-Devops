99fe72fa2797a93cc50b500406f7c4a1b76bbbbf
distribuidos <lewisalvarez99@gmail.com>
2020-11-19 18:16:58 -0800

    Commit

--- Infra/conf_Terraform/output.tf
+++ Infra/conf_Terraform/output.tf
@@ -5,3 +5,8 @@ output "availability_zone" {
         var.availability_zones
     ]
 }
+
+#Esta es la salida para que tiene la URL de del ELB para ingresar al portal
+output "elb_public_dns" {
+    value = aws_lb.aik_lb.dns_name
+}
